34d6c33378d5ee9e47059f233e3fc2a5f74cdaa5
ryunosuke_gashi <ryunosuke2001idaten@gmail.com>
2020-03-18 15:02:06 +0700

    share_time.vol.10

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
         Schema::defaultStringLength(191);
     }
 }
